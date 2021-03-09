75e900638aa9cbb47df51f69e911454cb53ae465
Banti Agrawal <banti.agrawal@gmail.com>
2021-03-10 00:14:10 +0100

    clean up

--- blockchain/apis/filters.py
+++ blockchain/apis/filters.py
@@ -2,17 +2,13 @@ from django_filters import rest_framework as filters
 from .models import HistoryPriceView, LatestPriceView
 from datetime import datetime, timedelta
 
-class LatestPriceViewFilter(filters.FilterSet):
-    days = filters.NumberFilter(field_name='latest_load_time', method='get_days', label="days")
-    def get_days(self, queryset, field_name, value):
-        return queryset.filter(latest_load_time__gte=datetime.now()-timedelta(days=int(value)))
-        
+class LatestPriceViewFilter(filters.FilterSet):        
     class Meta:
         model = LatestPriceView
-        fields = ['currency','days']
+        fields = ['currency']
 
 class HistoryPriceViewFilter(filters.FilterSet):
-    days = filters.NumberFilter(field_name='load_date_time', method='get_days', label="days")
+    days = filters.NumberFilter(field_name='load_date_time', method='_days', label="days")
 
     def __init__(self, *args, **kwargs):
         kwargs['data']._mutable = True
@@ -21,9 +17,9 @@ class HistoryPriceViewFilter(filters.FilterSet):
         if 'id' not in kwargs['data']:
             kwargs['data']['id'] = 1
         kwargs['data']._mutable = False
-
         super(HistoryPriceViewFilter, self).__init__(*args, **kwargs)
-    def get_days(self, queryset, field_name, value):
+    
+    def _days(self, queryset, field_name, value):
         return queryset.filter(load_date_time__gte=datetime.now()-timedelta(days=int(value)))
 
     class Meta:
